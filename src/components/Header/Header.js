7da8a971c3af30127b3aebb33ac2aa976e984dde
Ale-we <liss_alexandra@yahoo.com>
2020-09-11 19:27:37 +0300

    added logo to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,16 @@
 import React from "react" ;
+import { AppLogo } from "./AppLogo";
+
 
 import "./Header.css";
+import { MovieDetails } from "./MovieDetails";
+import { MovieSearch }  from "./MovieSearch";
+
 
 export function Header() {
-    return <div className = "header"></div>
+    return <div className = "header">
+        <AppLogo />
+        <MovieDetails />
+        <MovieSearch />
+    </div>
 }
