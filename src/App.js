2cfc5b206cae9041c8296dbb6b39622115d4a52b
AndreiMaresDan <andrei.mares911@gmail.com>
2020-09-15 17:48:45 +0300

    almost done the Create Account

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 // import { HomePage } from "./Pages/HomePage";
 import { LogIn } from "./Pages/LogIn";
-// import { CreateAccount } from "./Pages/CreateAccount"
+import { CreateAccount } from "./Pages/CreateAccount"
 
 function App() {
   return (
     <div className="App">
     {/* < HomePage /> */}
     < LogIn />
-    {/* < CreateAccount /> */}
+    < CreateAccount />
     </div>
   );
 }
--- src/Pages/CreateAccount.js
+++ src/Pages/CreateAccount.js
@@ -2,7 +2,16 @@ import React from "react" ;
 
 import "./CreateAccount.css";
 import { Header } from "../components/Header/Header";
+import { CreateAccountBody } from "./CreateAccountComponents/CreateAccountBody";
 
-export function CreateAccount() {
-    return <Header />
-}
+export function CreateAccount(props) {
+    return (
+        <div className="background-image">
+        
+            <Header />
+        <div className="CreateAccount-container">   
+            <CreateAccountBody />
+        </div>
+        </div>
+        )
+    }
