ce8b0ea5c9996e959caa5d8bd7e58a71fb8335b1
AndreiMaresDan <andrei.mares911@gmail.com>
2020-09-16 17:23:41 +0300

    added function to Movie Search and Details

--- src/components/Header/MovieSearch.js
+++ src/components/Header/MovieSearch.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Search from "../../Search.png"
 
 import "./MovieSearch.css";
 
 export function MovieSearch() {
-    return <img src={Search} alt ="search" className="search-logo" />
+    return ( <Link to = "/MovieSearch"> 
+        <img src={Search} alt ="search" className="search-logo" />
+        </Link>)
 }
