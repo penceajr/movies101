0f81299088846fb4409ae36fa75c3f0a8ad933f8
AndreiMaresDan <andrei.mares911@gmail.com>
2020-09-19 22:02:40 +0300

    Search Movie Page added WIP

--- src/components/Header/MovieSearch.js
+++ src/components/Header/MovieSearch.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Search from "../../Search.png";
+import { SearchMoviePage } from "../../Pages/SearchMoviePage"
 
 import "./MovieSearch.css";
 
