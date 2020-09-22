e74e7a0c9aa43169a3929971ac49b3239d336381
penceajr <penceajr@yahoo.com>
2020-09-22 18:10:59 +0100

    rendered input and details based on user state

--- src/Pages/MovieDetails.js
+++ src/Pages/MovieDetails.js
@@ -1,12 +1,15 @@
 import React from "react"
-import { InputBar } from "../components/Header/Input"
+import { Link } from "react-router-dom"
 import "./MovieDetails.css"
 
 
 export class MovieDetailsPage extends React.Component {
     state = { 
           UserState: false,
-          MovieInformation: {}
+          MovieInformation: {},
+          EditButtonState: false,
+          MovieID: "",
+          redirect: false
         }   
 
 getMovieInformation = (id) =>{
@@ -22,9 +25,7 @@ getMovieInformation = (id) =>{
 }
 
 componentDidMount(){
-    this.handleUserState()
     const [search ,id ] = this.props.location.search.split("=");  
-    console.log(search);
     this.getMovieInformation(id)
 }
 
@@ -34,6 +35,74 @@ handleUserState = () =>{
             UserState: true
         })
     }
+}
+
+handleDeleteButton = ()=>{
+    fetch(`https://movies-app-siit.herokuapp.com/movies/${this.state.MovieID}`, {
+            method: "DELETE",
+            mode: "cors",
+            headers: {
+                "X-Auth-Token": localStorage.getItem("accessToken")
+                },
+          })
+            .then((res) => res.json())
+            .then((json) => {
+              console.log(json);
+              localStorage.removeItem("bestRatedMovies");
+              window.location.reload();
+            });
+}
+renderEditStateIfUserLoggedIn = ()=>{
+        if (localStorage.getItem("accessToken")) { 
+            return(
+            <div>   
+                {this.handleRenderingButtons()}
+            </div>
+            )}
+}
+handleEditButton = () => {
+    this.setState({EditButtonState:true})
+    this.handleUserState()
+}
+handleCancelButton = () =>{
+    this.setState({EditButtonState: false, UserState:false})
+}
+handleSaveButton = () => {
+    fetch(`https://movies-app-siit.herokuapp.com/movies/${this.state.MovieID}`, {
+      method: "PUT",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "X-Auth-Token": localStorage.getItem("accessToken"),
+      },
+      body: JSON.stringify({
+        Poster: "Stefan cel Mare",
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+      });
+}
+handleRenderingButtons = () => {
+    if(!this.state.EditButtonState){
+      return (
+      <div>
+      <button onClick={this.handleEditButton}>Edit</button>
+      <Link to="/">
+      <button onClick={this.handleDeleteButton}>Delete</button>
+      </Link>
+      </div>
+      )
+    } else{
+       return (
+       <div>
+       <button onClick={this.handleSaveButton}>Save</button>
+       <button onClick={this.handleCancelButton}>Cancel</button>
+       </div>
+       )
+    } 
 }
 
     render(){
@@ -47,92 +116,87 @@ handleUserState = () =>{
                 <div className="Poster-Container">
                     <img src={this.state.MovieInformation.Poster} alt="poster" className="poster-img"></img>
                 </div>
+                <div className="edit-and-delete-buttons">
+                     {this.renderEditStateIfUserLoggedIn()}
+                 </div>
                 <div className="MovieInformationContainer">
+                    
                     <div className="title-container">
-                        <InputBar 
-                        LabelcssClass="MovieInformationContainer-label" 
-                        InputcssClass={"MovieInformationContainer-input" + (this.state.UserState ? " show" : " hidden")} 
-                        label="Title" type="text" placeholder="Movie Title"/>
-                        <p className={(!this.state.UserState ? " show" : " hidden")}>{this.state.MovieInformation.Title}</p>
+                        
+                         <p className="MovieInformationContainer-label">Title: {(this.state.UserState ? "" : this.state.MovieInformation.Title)}
+                             <input className={"MovieInformationContainer-input"+(this.state.UserState ? " show" : ' hidden')}>
+                            </input>
+                        </p>
                     </div> 
 
                     <div className="year-container">  
-                        <InputBar 
-                        LabelcssClass="MovieInformationContainer-label" 
-                        InputcssClass={"MovieInformationContainer-input" + (this.state.UserState ? " show" : " hidden")} 
-                        label="Year" type="text" placeholder="Year"/>
-                        <p className={(!this.state.UserState ? " show" : " hidden")}>{this.state.MovieInformation.Year}</p>
+                        <p className="MovieInformationContainer-label">Year: {(this.state.UserState ? "" : this.state.MovieInformation.Year)}
+                             <input className={"MovieInformationContainer-input"+(this.state.UserState ? " show" : ' hidden')}>
+                            </input>
+                        </p>
                     </div> 
 
                     <div className="imageURL-container">
-                    <InputBar 
-                        LabelcssClass="MovieInformationContainer-label" 
-                        InputcssClass={"MovieInformationContainer-input" + (this.state.UserState ? " show" : " hidden")} 
-                        label="ImageURL" type="text" placeholder="ImageURL"/>
-                        <p className={(!this.state.UserState ? " show" : " hidden")}>{this.state.MovieInformation.Poster}</p>
+                        <p className={`MovieInformationContainer-label " ${(this.state.UserState ? " show" : ' hidden')}`}>
+                            Image URL: {(this.state.UserState ? "" : this.state.MovieInformation.Poster)}
+                        <input className={"MovieInformationContainer-input"+(this.state.UserState ? " show" : ' hidden')}>
+                            </input>
+                        </p>
                     </div> 
 
                     <div className="released-container"> 
-                        <InputBar  
-                        LabelcssClass="MovieInformationContainer-label" 
-                        InputcssClass={"MovieInformationContainer-input" + (this.state.UserState ? " show" : " hidden")} 
-                        label="Released" type="text" placeholder="Released"/>
-                        <p className={(!this.state.UserState ? " show" : " hidden")}>{this.state.MovieInformation.Released}</p>
+                        <p className="MovieInformationContainer-label">Released: {(this.state.UserState ? "" : this.state.MovieInformation.Released)}
+                             <input className={"MovieInformationContainer-input"+(this.state.UserState ? " show" : ' hidden')}>
+                            </input>
+                        </p>
                     </div>
 
                     <div className="runtime-container">
-                        <InputBar 
-                        LabelcssClass="MovieInformationContainer-label" 
-                        InputcssClass={"MovieInformationContainer-input" + (this.state.UserState ? " show" : " hidden")} 
-                        label="Runtime" type="text" placeholder="Runtime"/>
-                        <p className={(!this.state.UserState ? " show" : " hidden")}>{this.state.MovieInformation.Runtime}</p>
+                        <p className="MovieInformationContainer-label">Runtime: {(this.state.UserState ? "" : this.state.MovieInformation.Runtime)}
+                             <input className={"MovieInformationContainer-input"+(this.state.UserState ? " show" : ' hidden')}>
+                            </input>
+                        </p>
                     </div>
                     <div className="genre-container">
-                        <InputBar 
-                        LabelcssClass="MovieInformationContainer-label" 
-                        InputcssClass={"MovieInformationContainer-input" + (this.state.UserState ? " show" : " hidden")} 
-                        label="Genre" type="text" placeholder="Genre"/>
-                        <p className={(!this.state.UserState ? " show" : " hidden")}>{this.state.MovieInformation.Genre}</p>
+                        <p className="MovieInformationContainer-label">Genre: {(this.state.UserState ? "" : this.state.MovieInformation.Genre)}
+                             <input className={"MovieInformationContainer-input"+(this.state.UserState ? " show" : ' hidden')}>
+                            </input>
+                        </p>
                     </div>
 
                     <div className="director-container">
-                        <InputBar 
-                        LabelcssClass="MovieInformationContainer-label" 
-                        InputcssClass={"MovieInformationContainer-input" + (this.state.UserState ? " show" : " hidden")} 
-                        label="Director" type="text" placeholder="Director"/>
-                        <p className={(!this.state.UserState ? " show" : " hidden")}>{this.state.MovieInformation.Director}</p>
+                        <p className="MovieInformationContainer-label">Director: {(this.state.UserState ? "" : this.state.MovieInformation.Director)}
+                             <input className={"MovieInformationContainer-input"+(this.state.UserState ? " show" : ' hidden')}>
+                            </input>
+                        </p>
                     </div>
 
                     <div className="actors-container">
-                        <InputBar 
-                        LabelcssClass="MovieInformationContainer-label" 
-                        InputcssClass={"MovieInformationContainer-input" + (this.state.UserState ? " show" : " hidden")} 
-                        label="Actors" type="text" placeholder="Actors"/>
-                        <p className={(!this.state.UserState ? " show" : " hidden")}>{this.state.MovieInformation.Actors}</p>
+                        <p className="MovieInformationContainer-label">Actors: {(this.state.UserState ? "" : this.state.MovieInformation.Actors)}
+                             <input className={"MovieInformationContainer-input"+(this.state.UserState ? " show" : ' hidden')}>
+                            </input>
+                        </p>
                     </div>
 
                     <div className="plot-container">
-                        <InputBar 
-                        LabelcssClass="MovieInformationContainer-label" 
-                        InputcssClass={"MovieInformationContainer-input" + (this.state.UserState ? " show" : " hidden")} 
-                        label="Plot" type="text" placeholder="Plot"/>
-                        <p className={(!this.state.UserState ? " show" : " hidden")}>{this.state.MovieInformation.Plot}</p>
+                        <p className="MovieInformationContainer-label">Plot: {(this.state.UserState ? "" : this.state.MovieInformation.Plot)}
+                             <input className={"MovieInformationContainer-input"+(this.state.UserState ? " show" : ' hidden')}>
+                            </input>
+                        </p>
                     </div>
 
                     <div className="country-container">
-                        <InputBar 
-                        LabelcssClass="MovieInformationContainer-label" 
-                        InputcssClass={"MovieInformationContainer-input" + (this.state.UserState ? " show" : " hidden")} 
-                        label="Country" type="text" placeholder="Country"/>
-                        <p className={(!this.state.UserState ? " show" : " hidden")}>{this.state.MovieInformation.Country}</p>
+                        <p className="MovieInformationContainer-label">Country: {(this.state.UserState ? "" : this.state.MovieInformation.Country)}
+                             <input className={"MovieInformationContainer-input"+(this.state.UserState ? " show" : ' hidden')}>
+                            </input>
+                        </p>
                     </div>
 
                     <div className="imdbRating-container">
-                        <InputBar 
-                        LabelcssClass="MovieInformationContainer-label" 
-                        InputcssClass={"MovieInformationContainer-input" + (this.state.UserState ? " show" : " hidden")} 
-                        label="imdbRating" type="text" placeholder="imdbRating"/>
-                        <p className={(!this.state.UserState ? " show" : " hidden")}>{this.state.MovieInformation.imdbRating}</p>
+                        <p className="MovieInformationContainer-label">imdbRating: {(this.state.UserState ? "" : this.state.MovieInformation.imdbRating)}
+                             <input className={"MovieInformationContainer-input"+(this.state.UserState ? " show" : ' hidden')}>
+                            </input>
+                        </p>
                     </div>
             </div>
         </div>
